67480d7af5f2620c8b66c96e76ae4c2d72b13407
Njel <Njel@mail.com>
2014-02-20 07:18:31 -0500

    02/19/2014

--- client/views/posts/post_totals.js
+++ client/views/posts/post_totals.js
@@ -8,63 +8,101 @@ Template.postTotals.helpers({
   	var start = this.year + '-' + sm + '-01';
   	var end = this.year + '-' + em + '-01';
 
-  	var e = {T: 0, C: 0, S: 0, R: 0, M: 0, Tot: 0};
-  	var h = {T: 0.0, C: 0.0, S: 0.0, R: 0.0, M: 0.0, Tot: 0.0, N: 0.0, S100: 0.0, S125: 0.0, S150: 0.0, S200: 0.0};
+    var e = [];
+    var h = [];
+    var types = EventTypes.find({active: true});
+    types.forEach(function(t) {
+      e[t._id] = 0;
+      h[t._id] = 0.0;
+    });
+    e['Tot'] = 0;
+    h['Tot'] = 0.0;
+    h['N'] = 0.0;
+    h['X100'] = 0.0;
+    h['X125'] = 0.0;
+    h['X150'] = 0.0;
+    h['X200'] = 0.0;
+
+  	// var e = {T: 0, C: 0, S: 0, R: 0, M: 0, Tot: 0};
+  	// var h = {T: 0.0, C: 0.0, S: 0.0, R: 0.0, M: 0.0, Tot: 0.0, N: 0.0, S100: 0.0, S125: 0.0, S150: 0.0, S200: 0.0};
 
   	events = Events.find({postId: this._id, start: {$gte: start, $lt: end}});
   	events.forEach(function(i) {
   	  e[i.type] += 1;
   	  e['Tot'] += 1;
 
-  	  if(i.type == 'T') {
+  	  if(e[i.type].code == 'W' || e[i.type].code == 'T') {
   	  	var cH = calcHours(i.start, i.end, i.allDay, 12, 00);
-  	  	h['T'] += cH['t'];
-  	  	h['N'] += cH['n'];
-  	  	h['S100'] += cH['s100'];
-  	  	h['S125'] += cH['s125'];
-  	  	h['S150'] += cH['s150'];
-  	  	h['S200'] += cH['s200'];
-  	  	h['Tot'] += cH['t'];
+  	  	h[i.type] += cH['hrs'];
+        h['N'] += cH['N'];
+        h['X100'] += cH['X100'];
+        h['X125'] += cH['X125'];
+        h['X150'] += cH['X150'];
+        h['X200'] += cH['X200'];
+        h['Tot'] += cH['hrs'];
   	  } else {
   	    if(i.allDay) {
-  	  	  h[i.type] += 7.5;
-  	  	  h['Tot'] += 7.5;
+          h[i.type] += 7.5;
+          h['Tot'] += 7.5;
   	    } else {
   	  	  var d = (new Date(i.end) - new Date(i.start)) / 1000 / 60 / 60;
-   	  	  h[i.type] += d;
-   	  	  h['Tot'] += d;
+          h[i.type] += d;
+          h['Tot'] += d;
    	  	}
   	  }
   	});
 
   	// console.log(calcHours("2014-02-06T22:30:00.000Z","2014-02-07T02:30:00.000Z",false,17,00));
-  	return {evts: e, hours: h};
+
+    var results = new Meteor.Collection(null);
+    var types = EventTypes.find({active: true});
+    types.forEach(function(t) {
+      results.insert({
+        code: t.code,
+        txtColor: t.textColor,
+        bgColor: t.backgroundColor,
+        E: e[t._id],
+        H: h[t._id],
+        width: '30px'
+      });
+    });
+
+    results.insert({
+      code: 'Tot',
+      txtColor: '#fff',
+      bgColor: '#000',
+      E: e['Tot'],
+      H: h['Tot'],
+      width: '40px'
+    });
+
+  	return {results: results.find()};
   }
 });
 
 function calcHours(start, end, allDay, supHH, supMM) {
-  var t = 0.0;
-  var n = 0.0;
-  var s100 = 0.0;
-  var s125 = 0.0;
-  var s150 = 0.0;
-  var s200 = 0.0;
+  var T = 0.0;
+  var N = 0.0;
+  var X100 = 0.0;
+  var X125 = 0.0;
+  var X150 = 0.0;
+  var X200 = 0.0;
 
   var sd = new Date(start);
 
   if(allDay) {
-    t = 7.5;
-    n = 7.5;
+    T = 7.5;
+    N = 7.5;
     if(sd.getDay() == 0 || sd.getDay() == 6) {
-      s200 = h;
+      X200 = N;
     }
   } else {
 	var ed = new Date(end);
 
-    t = (ed - sd) / 1000 / 60 / 60;
+    T = (ed - sd) / 1000 / 60 / 60;
 
     if(sd.getDay() == 0 || sd.getDay() == 6) {
-      s200 = t;
+      X200 = T;
     } else {
 	  var y = sd.getFullYear();
 	  var m = sd.getMonth();
@@ -81,15 +119,15 @@ function calcHours(start, end, allDay, supHH, supMM) {
 	  // console.log('T150 ' + T150);
 
 	  if(sd < T100)
-	  	n = (Math.min(ed, T100) - sd) / 1000 / 60 / 60;
+	  	N = (Math.min(ed, T100) - sd) / 1000 / 60 / 60;
 	  if(sd < T125 && ed > T100)
-	   	s100 = (Math.min(ed, T125) - Math.max(sd, T100)) / 1000 / 60 / 60;
+	   	X100 = (Math.min(ed, T125) - Math.max(sd, T100)) / 1000 / 60 / 60;
 	  if(sd < T150 && ed > T125)
-	   	s125 = (Math.min(ed, T150) - Math.max(sd, T125)) / 1000 / 60 / 60;
+	   	X125 = (Math.min(ed, T150) - Math.max(sd, T125)) / 1000 / 60 / 60;
 	  if(ed > T150)
-	    s150 = (ed - Math.max(sd, T150)) / 1000 / 60 / 60;
+	    X150 = (ed - Math.max(sd, T150)) / 1000 / 60 / 60;
 	}
   }
 
-  return {t: t, n: n, s100: s100, s125: s125, s150: s150, s200: s200};
+  return {hrs: T, N: N, X100: X100, X125: X125, X150: X150, X200: X200};
 }
