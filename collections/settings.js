160f791a4001bc1e972f1a80172c7395952afc31
Njel <Njel@mail.com>
2014-02-25 22:54:49 -0500

    02/25/2014

--- collections/settings.js
+++ collections/settings.js
@@ -1,7 +1,19 @@
 Settings = new Meteor.Collection('settings');
 
 Settings.allow({
-  insert: isAdminById,
-  update: isAdminById,
-  remove: isAdminById
+  // insert: isAdminById,
+  // update: ownsDocument,
+  // remove: ownsDocument
+  insert: function(userId, doc) {
+   // only allow posting if you are logged in
+   return !! userId;
+  },
+  update: function(userId, doc) {
+   // only allow posting if you are logged in
+   return !! userId;
+  },
+  remove: function(userId, doc) {
+   // only allow posting if you are logged in
+   return !! userId;
+  }
 });
